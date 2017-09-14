1002614f5e0e04a0e0413dc5275acfb897012435
Cory Holdom <CABAL@d-i89-241-183.student.eduroam.uq.edu.au>
2017-09-14 12:24:05 +1000

    Constant spike gen working

--- python/spikegen.py
+++ python/spikegen.py
@@ -1,6 +1,13 @@
+import base_class_version
+
+import random
+
+import numpy as np
+import pprint
+
 class Spike:
-    def __init__(self, **parameters):
-        self.__dict__.update(parameters)
+    def __init__(self):
+        pass
 
     def assign_parameters(self, **parameters):
         """
@@ -11,7 +18,7 @@ class Spike:
 
         rate: Spike rate (Hz)
         train_length: Duration of a single train (sec)
-        mode:
+        mode: The behaviour of the fire rate
              - "constant"
              - "bursty"
              - "random"
@@ -24,23 +31,57 @@ class Spike:
                  - Need to supply a tuple containing two scalars - "burst":
                      - Average number of spikes per burst
                      - Average length of rest (interspike duration) (sec)
-            random:
+            noisy:
                  - Train is full of noise, not "real" spikes
         repeats: The number of trains in the interval
         """
         for i in parameters:
-            setattr(self,i,parameters[i])
+            setattr(self, i, parameters[i])
 
-        print(self.__dict__.items())
+    def gen_spike_train(self):
+        try:
+            # Assign the number of times to repeat the train
+            self.repeats = 1 if not self.repeats else self.repeats
+
+            spikes = np.zeros(shape=(self.repeats, self.train_length))
+            print(spikes, self.repeats, np.shape(spikes))
+
+        except ValueError:
+            print(f"Number of repeats is {self.repeats}. Should be int > 0.\n"
+                   "Unable to generate spike train.")
+            return None
+
+        except TypeError:
+            print(f"Number of repeats is {self.repeats}. Should be int > 0.\n"
+                   "Unable to generate spike train.")
+            return None
+
+        if self.mode.lower() == "constant":
+            spike_count_array = np.random.poisson(self.rate * self.train_length, (1, self.repeats))[0]
+
+            spikes = []
+
+            print(spike_count_array, "cat")
+
+            # Create a nested list containing the times of each spike
+
+            for i in range(0, self.repeats):
+                spike_times = sorted([random.random() * self.train_length for j in range(spike_count_array[i])])
+                spikes += [spike_times]
+
+
+            pprint.pprint(spikes)
+            print(np.shape(spikes))
+
 
 audio_parameters = {
-        "rate": 5,
-        "train_length": 10,
-        "mode": "bursty",
-        "burst": (3,1)
+        "rate": 10,
+        "train_length": 5,
+        "mode": "constant",
+        "burst": (3, 1),
+        "repeats": 2
 }
 
-burst = Spike(rate = 0.5)
+burst = Spike()
 burst.assign_parameters(**audio_parameters)
-
-
+burst.gen_spike_train()

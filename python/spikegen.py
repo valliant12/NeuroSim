33375bf5d38a753f7aaff13a1940d852c60aa241
Cory Holdom <CABAL@d-i89-240-166.student.eduroam.uq.edu.au>
2017-09-20 09:40:29 +1000

    Burst work

--- python/spikegen.py
+++ python/spikegen.py
@@ -2,6 +2,7 @@ import base_class_version
 
 import random
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pprint
 
@@ -20,7 +21,7 @@ class Spike:
         train_length: Duration of a single train (sec)
         mode: The behaviour of the fire rate
              - "constant"
-             - "bursty"
+             - "burst"
              - "random"
 
             constant:
@@ -28,7 +29,7 @@ class Spike:
             bursty:
                  - Spikes occur in rapid bursts, separated by regions of
                  reduced activity
-                 - Need to supply a tuple containing two scalars - "burst":
+                 - Need to supply a tuple containing two scalars - "burst_parameters":
                      - Average number of spikes per burst
                      - Average length of rest (interspike duration) (sec)
             noisy:
@@ -36,7 +37,9 @@ class Spike:
         repeats: The number of trains in the interval
         """
         for i in parameters:
-            setattr(self, i, parameters[i])
+            setattr(self, i.lower(), parameters[i])
+
+        print(self.__repr__())
 
     def gen_spike_train(self):
         try:
@@ -66,22 +69,55 @@ class Spike:
             # Create a nested list containing the times of each spike
 
             for i in range(0, self.repeats):
-                spike_times = sorted([random.random() * self.train_length for j in range(spike_count_array[i])])
+                spike_times = sorted([random.random() * self.train_length for\
+                    j in range(spike_count_array[i])])
                 spikes += [spike_times]
 
-
             pprint.pprint(spikes)
-            print(np.shape(spikes))
+
+            return spikes
+
+        if self.mode.lower() == "burst":
+            try:
+                # Are burst parameters defined?
+                spikes_per_burst = self.burst_parameters[0]
+                interspike_duration = self.burst_parameters[1]
+
+            except AttributeError:
+                spikes_per_burst = 1
+                interspike_duration = 0.1
+                print(f"No burst parameters defined, assuming ",
+                       "{spikes_per_burst} spikes per burst and interspike",
+                       "duration of {interspike_duration}.")
+
+            burst_freq = self.rate / spikes_per_burst
+
+            for i in range(0, self.repeats):
+                burst_count = np.random.poisson(burst_freq * self.train_length)
+                burst_list = []
+
+                for j in range(burst_count):
+                    spike_count_array = np.random.poisson(spikes_per_burst)
+
+                    if spike_count_array > 0:
+                        burst_list[j] =
+
+                print(burst_count)
+
+
 
 
 audio_parameters = {
+        "identity": "audio",
         "rate": 10,
         "train_length": 5,
-        "mode": "constant",
-        "burst": (3, 1),
+        "mode": "BUrsT",
+        #"burst_parameters": (3, 1),
         "repeats": 2
 }
 
 burst = Spike()
 burst.assign_parameters(**audio_parameters)
-burst.gen_spike_train()
+burst.spikes = burst.gen_spike_train()
+
+
